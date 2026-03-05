995d5550ad600239fc88d43b7f3dde5fce7807c7
Lakshay Goyal <112741354+lakshay-goyal@users.noreply.github.com>
2026-03-05 14:22:09 +0530

    [Fix] : (Constant) Remove the logic of fetching the constant values from backend.

--- src/routes/frontend-constant.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getConstants, updateConstant } from "../controllers/frontend-constant.controller";
-
-const router = Router();
-
-router.get("/", getConstants);
-router.post("/", updateConstant);
-
-export default router;
